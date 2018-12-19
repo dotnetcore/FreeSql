751b2405330e147300fdd4fbe192e1379c00465a
28810 <28810@YEXIANGQIN>
2018-12-19 12:06:54 +0800

    SqlServer CodeFirst 适配完成

--- FreeSql.Tests/g.cs
+++ FreeSql.Tests/g.cs
@@ -6,8 +6,7 @@ using System.Text;
 public class g {
 
 	public static IFreeSql mysql = new FreeSql.FreeSqlBuilder()
-		//.UseConnectionString(FreeSql.DataType.MySql, "Data Source=127.0.0.1;Port=3306;User ID=root;Password=root;Initial Catalog=cccddd;Charset=utf8;SslMode=none;Max pool size=10")
-		.UseConnectionString(FreeSql.DataType.MySql, "Data Source=123.207.16.102;Port=3306;User ID=root;Password=qwe369258/*-;Initial Catalog=ManagerDB;Charset=utf8;SslMode=none;Max pool size=10")
+		.UseConnectionString(FreeSql.DataType.MySql, "Data Source=127.0.0.1;Port=3306;User ID=root;Password=root;Initial Catalog=cccddd;Charset=utf8;SslMode=none;Max pool size=10")
 		.Build();
 
 	public static IFreeSql sqlserver = new FreeSql.FreeSqlBuilder()
