5e0cb1eae10885b65f12f366276ee517816f0a60
2881099 <2881099@qq.com>
2019-01-30 22:11:55 +0800

    ThreadLocal修改使用方式

--- FreeSql/FreeUtil.cs
+++ FreeSql/FreeUtil.cs
@@ -10,7 +10,7 @@ using System.Threading;
 public static class FreeUtil {
 
 	private static DateTime dt1970 = new DateTime(1970, 1, 1);
-	private static ThreadLocal<Random> rnd = new ThreadLocal<Random>();
+	private static ThreadLocal<Random> rnd = new ThreadLocal<Random>(() => new Random());
 	private static readonly int __staticMachine = ((0x00ffffff & Environment.MachineName.GetHashCode()) +
 #if NETSTANDARD1_5 || NETSTANDARD1_6
 			1
