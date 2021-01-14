02785f4b8aabf12daa60300cab9b03c2e784e261
2881099 <2881099@qq.com>
2021-01-14 13:24:11 +0800

    trim code

--- FreeSql.DbContext/DbSet/DbSetAsync.cs
+++ FreeSql.DbContext/DbSet/DbSetAsync.cs
@@ -243,7 +243,8 @@ namespace FreeSql
 
                         if (curList.Any() == false) //全部删除
                         {
-                            var delall = _db.OrmOriginal.Delete<object>().AsType(tref.RefMiddleEntityType)
+                            var delall = _db.OrmOriginal.Delete<object>()
+                                .AsType(tref.RefMiddleEntityType)
                                 .WithTransaction(_uow?.GetOrBeginTransaction());
                             foreach (var midWhere in midWheres) delall.Where(midWhere);
                             var sql = delall.ToSql();
