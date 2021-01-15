027f3fcce01d095ebf98d271e69cad75b86d5d87
2881099 <2881099@qq.com>
2021-01-15 18:59:53 +0800

    - 优化 DbSet/Repository 级联保存时，忽略未配置的导航属性；

--- FreeSql.DbContext/DbSet/DbSetSync.cs
+++ FreeSql.DbContext/DbSet/DbSetSync.cs
@@ -217,7 +217,7 @@ namespace FreeSql
                 if (_table.ColumnsByCsIgnore.ContainsKey(prop.Name)) return;
                 if (_table.ColumnsByCs.ContainsKey(prop.Name)) return;
 
-                var tref = _table.GetTableRef(prop.Name, true);
+                var tref = _table.GetTableRef(prop.Name, false); //防止非正常的导航属性报错
                 if (tref == null) return;
                 switch (tref.RefType)
                 {
