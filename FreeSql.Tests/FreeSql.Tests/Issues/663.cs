fca4c884d7890d2109fe27d17b6afac14a3b3e53
2881099 <2881099@qq.com>
2021-01-22 18:57:31 +0800

    add test code #663

---
+++ FreeSql.Tests/FreeSql.Tests/Issues/663.cs
@@ -0,0 +1,55 @@
+﻿using FreeSql.DataAnnotations;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace FreeSql.Tests.Issues
+{
+	public class _663
+	{
+		[Fact]
+		public void MySqlInsertOrUpdate()
+		{
+            var fsql = g.mysql;
+			var rst = fsql.Insert<Song_Tag>(new[] { new Song_Tag { SongId = 1, TagId = 1 } })
+				.NoneParameter()
+				.MySqlIgnoreInto()
+				.ToSql();
+
+			Assert.Equal(@"INSERT IGNORE INTO `Song_Tagg663`(`SongId`, `TagId`) VALUES(1, 1)", rst);
+			
+		}
+
+		[Table(Name = "Song663")]
+		class Song
+		{
+			[Column(IsIdentity = true)]
+			public int Id { set; get; }
+			public string Text { set; get; }
+			public List<Tag> Tags { set; get; }
+		}
+		[Table(Name = "Song_Tagg663")]
+		class Song_Tag
+		{
+			public int SongId { set; get; }
+			public int TagId { set; get; }
+			public Song Song { set; get; }
+			public Tag Tag { set; get; }
+		}
+		[Table(Name = "Tag663")]
+		class Tag
+		{
+			[Column(IsIdentity = true)]
+			public int Id { set; get; }
+			public string Text { set; get; }
+			public List<Song> Songs { set; get; }
+		}
+	}
+}
