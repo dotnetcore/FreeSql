82c9d1e12a5147b38b0e6a3bf945772829b09064
luoyunchong <luoyunchong@foxmail.com>
2021-06-01 19:42:19 +0800

    #783 fix ExecuteMySqlBulkCopyAsync .net core 3.1 Method not found

---
+++ FreeSql.Tests/FreeSql.Tests.Provider.MySqlConnector/MySqlConnector/FreeSqlMySqlConnectorGlobalExtensionsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FreeSql.Tests.Provider.MySqlConnector
+{
+    public class FreeSqlMySqlConnectorGlobalExtensionsTest
+    {
+        class BulkCopyValue
+        {
+            public Guid id { get; set; }
+            public DateTime createtime { get; set; }
+        }
+        [Fact]
+        public async Task ExecuteMySqlBulkCopyAsync()
+        {
+            var fsql = g.mysql;
+            fsql.CodeFirst.SyncStructure<BulkCopyValue>();
+
+            List<BulkCopyValue> bulkCopyValues = new List<BulkCopyValue>();
+            for (var i = 0; i < 1000; i++)
+            {
+                bulkCopyValues.Add(new BulkCopyValue() { createtime = DateTime.Now });
+            }
+            await fsql.Insert<BulkCopyValue>().AppendData(bulkCopyValues).ExecuteMySqlBulkCopyAsync();
+        }
+    }
+}
