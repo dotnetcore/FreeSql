9c4ea7ef89ce1216f4ad464a346ea3b313a2efaa
CLKCLK <617682513@qq.com>
2021-09-14 11:04:53 +0800

    修复: sqlserver row_number排序不一致
    sqlserver 使用row_number进行分页时 第一页和后续几页排序不一致的问题

--- Providers/FreeSql.Provider.Odbc/SqlServer/Curd/OdbcSqlServerSelect.cs
+++ Providers/FreeSql.Provider.Odbc/SqlServer/Curd/OdbcSqlServerSelect.cs
@@ -41,14 +41,15 @@ namespace FreeSql.Odbc.SqlServer
                 //if (_limit > 0) sb.Append("TOP ").Append(_skip + _limit).Append(" "); //TOP 会引发 __rownum__ 无序的问题
                 if (_skip <= 0 && _limit > 0) sb.Append("TOP ").Append(_limit).Append(" ");
                 sb.Append(field);
+
+                if (string.IsNullOrEmpty(_orderby))
+                {
+                    var pktb = _tables.Where(a => a.Table.Primarys.Any()).FirstOrDefault();
+                    if (pktb != null) _orderby = string.Concat(" \r\nORDER BY ", pktb.Alias, ".", _commonUtils.QuoteSqlName(pktb?.Table.Primarys.First().Attribute.Name));
+                    else _orderby = string.Concat(" \r\nORDER BY ", _tables.First().Alias, ".", _commonUtils.QuoteSqlName(_tables.First().Table.Columns.First().Value.Attribute.Name));
+                }
                 if (_skip > 0)
                 {
-                    if (string.IsNullOrEmpty(_orderby))
-                    {
-                        var pktb = _tables.Where(a => a.Table.Primarys.Any()).FirstOrDefault();
-                        if (pktb != null) _orderby = string.Concat(" \r\nORDER BY ", pktb.Alias, ".", _commonUtils.QuoteSqlName(pktb?.Table.Primarys.First().Attribute.Name));
-                        else _orderby = string.Concat(" \r\nORDER BY ", _tables.First().Alias, ".", _commonUtils.QuoteSqlName(_tables.First().Table.Columns.First().Value.Attribute.Name));
-                    }
                     sb.Append(", ROW_NUMBER() OVER(").Append(_orderby).Append(") AS __rownum__");
                 }
                 sb.Append(" \r\nFROM ");
