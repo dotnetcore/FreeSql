2e14568b533e730e5bb9ff41660b8d2fbec954bc
d4ilys <963922242@qq.com>
2023-11-21 09:54:47 +0800

    Clickhouse NoneCommandParameter测试

--- FreeSql.Tests/FreeSql.Tests/ClickHouse/ClickHouseTest3.cs
+++ FreeSql.Tests/FreeSql.Tests/ClickHouse/ClickHouseTest3.cs
@@ -20,7 +20,7 @@ namespace FreeSql.Tests.ClickHouse
             _fsql = new FreeSqlBuilder().UseConnectionString(DataType.ClickHouse,
                     "Host=192.168.1.123;Port=8123;Database=test;Compress=True;Min Pool Size=1")
                 .UseMonitorCommand(cmd => _output.WriteLine($"线程：{cmd.CommandText}\r\n"))
-                .UseNoneCommandParameter(true)
+                .UseNoneCommandParameter(false)
                 .Build();
         }
 
