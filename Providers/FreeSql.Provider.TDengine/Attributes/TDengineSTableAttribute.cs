93c6e62a823d900ff6152b8a62a2731abfb66c72
d4ilys <963922242@com>
2024-09-03 09:08:30 +0800

    FreeSql.Provider.TDengine初始化

---
+++ Providers/FreeSql.Provider.TDengine/Attributes/TDengineSTableAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeSql.DataAnnotations
+{
+    /// <summary>
+    /// TDengine 超级表
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TDengineSTableAttribute : Attribute
+    {
+    }
+}
---
+++ Providers/FreeSql.Provider.TDengine/TDengineExtensions.cs
@@ -0,0 +1,6 @@
+﻿using System.Text;
+
+public static class FreeSqlTDengineGlobalExtensions
+{
+
+}
