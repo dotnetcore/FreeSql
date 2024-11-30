73b17b26092cf4a76b6a85c71323774aa41aeb0e
d4ilys <963922242@com>
2024-11-30 11:15:58 +0800

    增加TDengine单元测试

--- FreeSql.Tests/FreeSql.Tests.Provider.TDengine/g.cs
+++ FreeSql.Tests/FreeSql.Tests.Provider.TDengine/g.cs
@@ -12,18 +12,13 @@ namespace FreeSql.Tests.Provider.TDengine
     {
         private static readonly Lazy<IFreeSql> tdengineLazy = new Lazy<IFreeSql>(() =>
         {
-             var fsql = new FreeSql.FreeSqlBuilder()
+            var fsql = new FreeSql.FreeSqlBuilder()
                 .UseConnectionString(FreeSql.DataType.TDengine,
                     "host=localhost;port=6030;username=root;password=taosdata;protocol=Native;db=test;")
-                .UseAutoSyncStructure(true)
-                .UseNameConvert(Internal.NameConvertType.ToLower)
-                .UseMonitorCommand(
-                    cmd => Trace.WriteLine("\r\n线程" + Thread.CurrentThread.ManagedThreadId + ": " +
-                                           cmd.CommandText) //监听SQL命令对象，在执行前
-                    //, (cmd, traceLog) => Console.WriteLine(traceLog)
-                )
+                .UseMonitorCommand(cmd => Console.WriteLine($"Sql：{cmd.CommandText}\r\n"))
+                .UseNoneCommandParameter(true)
                 .Build();
-             return fsql;
+            return fsql;
         });
 
         public static IFreeSql tdengine => tdengineLazy.Value;
