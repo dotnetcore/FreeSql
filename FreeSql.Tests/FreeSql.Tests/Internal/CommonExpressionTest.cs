01967e7656b1f841f183f47aa47a63b5612cb2f8
xieyidong <xieyidong@laiye.com>
2024-12-12 11:21:48 +0800

    fix:修复使用DateTime相减时，出现IIF无法转换表达式的错误

--- FreeSql.Tests/FreeSql.Tests/Internal/CommonExpressionTest.cs
+++ FreeSql.Tests/FreeSql.Tests/Internal/CommonExpressionTest.cs
@@ -4,7 +4,9 @@ using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FreeSql.DataAnnotations;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Xunit;
 
 namespace FreeSql.Tests.Internal
@@ -250,6 +252,7 @@ FROM ""IIFTest01Model"" a", sql);
             sql = fsql.Select<IIFTest01Model>().ToSql(a => a.BoolNullable.Value && a.Id > 0 && a.BoolNullable.Value ? 10 : 11);
             Assert.Equal(@"SELECT case when a.""BoolNullable"" = 1 AND a.""Id"" > 0 AND a.""BoolNullable"" = 1 then 10 else 11 end as1 
 FROM ""IIFTest01Model"" a", sql);
+
         }
 
         class IIFTest01Model
@@ -257,6 +260,69 @@ FROM ""IIFTest01Model"" a", sql);
             public int Id { get; set; }
             public bool Bool { get; set; }
             public bool? BoolNullable { get; set; }
+        }
+
+        [Fact]
+        public void IIFTest02()
+        {
+            var parameters = new List<DbParameter>();
+
+            var fsql = g.sqlite;
+            var sql = "";
+            var startTime = DateTime.UtcNow;
+            var query = fsql.Select<IIFTest02Model1, IIFTest02Model2>()
+                .WithParameters(parameters)
+                .InnerJoin((model1, model2) =>
+                    model2.StartDateTime == startTime
+                    && model1.StartTime <= model2.EndDateTime
+                    && model1.StopTime > model2.StartDateTime)
+                .GroupBy((model1, model2) => new
+                {
+                    model1.EntityId,
+                    model2.StartDateTime,
+                    model2.EndDateTime,
+                })
+                .WithTempQuery(group => new
+                {
+                    EntityId = group.Key.EntityId,
+                    DateTime = group.Key.StartDateTime,
+                    TotalRunTime = group.Sum<double>(((group.Value.Item1.StopTime > group.Key.EndDateTime ? group.Key.EndDateTime : group.Value.Item1.StopTime)
+                        - (group.Value.Item1.StartTime < group.Key.StartDateTime ? group.Key.StartDateTime : group.Value.Item1.StartTime)).TotalSeconds),
+                    Count = group.Count()
+                })
+                .WithParameters(parameters);
+
+            sql = query.ToSql();
+            Assert.Equal($@"SELECT * 
+FROM ( 
+    SELECT a.""EntityId"", b.""StartDateTime"" ""DateTime"", sum(((strftime('%s',case when a.""StopTime"" > b.""EndDateTime"" then b.""EndDateTime"" else a.""StopTime"" end)-strftime('%s',case when a.""StartTime"" < b.""StartDateTime"" then b.""StartDateTime"" else a.""StartTime"" end)))) ""TotalRunTime"", count(1) ""Count"" 
+    FROM ""IIFTest02Model1"" a 
+    INNER JOIN ""IIFTest02Model2"" b ON b.""StartDateTime"" = '{startTime.ToString("yyyy-MM-dd HH:mm:ss")}' AND a.""StartTime"" <= b.""EndDateTime"" AND a.""StopTime"" > b.""StartDateTime"" 
+    GROUP BY a.""EntityId"", b.""StartDateTime"", b.""EndDateTime"" ) a", sql);
+
+        }
+        public class IIFTest02Model1
+        {
+            public long _Id { get; set; }
+            public long Id { get; set; }
+
+
+            public long EntityId { get; set; }
+
+            public DateTime StartTime { get; set; }
+            public DateTime StopTime { get; set; }
+
+        }
+
+        /// <summary>
+        /// 任务统计日期
+        /// </summary>
+        public class IIFTest02Model2
+        {
+            public long Id { get; set; }
+            public DateTime StartDateTime { get; set; }
+            public DateTime EndDateTime { get; set; }
+
         }
     }
 }
