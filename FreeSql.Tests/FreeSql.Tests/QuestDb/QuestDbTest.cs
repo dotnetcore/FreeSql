704bb5fc6b96497963d9fc2b4038722ac244aa01
d4ilys <963922242@qq.com>
2025-10-16 09:49:34 +0800

    优化QuestDb IHttpClientFactory及IServiceCollection相关逻辑

--- FreeSql.Tests/FreeSql.Tests/QuestDb/QuestDbTest.cs
+++ FreeSql.Tests/FreeSql.Tests/QuestDb/QuestDbTest.cs
@@ -21,7 +21,7 @@ namespace FreeSql.Tests.QuestDb
             .UseConnectionString(FreeSql.DataType.QuestDb,
                 @"host=192.168.1.114;port=8812;username=admin;password=quest;database=qdb;ServerCompatibilityMode=NoTypeLoading;")
             .UseMonitorCommand(cmd => Debug.WriteLine($"Sql：{cmd.CommandText}")) //监听SQL语句
-            .UseQuestDbRestAPI("192.168.1.114:9000")
+            .UseQuestDbRestAPI("192.168.1.114:9000") 
             .Build();
 
     }
