e408c7f560090df1a3dcff0a3c0e87c15b8434d5
2881099 <2881099@qq.com>
2025-12-16 09:47:51 +0800

    - 增加 CreateUnitOfWork 从外部事务创建；

--- FreeSql.DbContext/Repository/Extensions/FreeSqlRepositoryExtensions.cs
+++ FreeSql.DbContext/Repository/Extensions/FreeSqlRepositoryExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using FreeSql;
 using System;
+using System.Data.Common;
 using System.Linq;
 using System.Linq.Expressions;
 
@@ -25,5 +26,17 @@ partial class FreeSqlDbContextExtensions
     public static IRepositoryUnitOfWork CreateUnitOfWork(this IFreeSql that)
     {
         return new RepositoryUnitOfWork(that);
+    }
+
+    /// <summary>
+    /// 创建基于外部事务的工作单元
+    /// <para>注意：该工作单元 Commit/Dispose 时不会关闭传入的 transaction，请自行管理 transaction 生命周期</para>
+    /// </summary>
+    /// <param name="fsql"></param>
+    /// <param name="transaction">外部传入的 DbTransaction</param>
+    /// <returns></returns>
+    public static IUnitOfWork CreateUnitOfWork(this IFreeSql fsql, DbTransaction transaction)
+    {
+        return new ExternalUnitOfWork(fsql, transaction);
     }
 }
