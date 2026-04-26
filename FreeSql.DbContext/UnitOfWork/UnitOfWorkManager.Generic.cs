91ec7f451123ac9e820bb09c041297fac0286c05
igeekfan <igeekfan@foxmail.com>
2026-04-26 18:41:15 +0800

    Add 泛型IUnitOfWorkManager接口

--- FreeSql.DbContext/UnitOfWork/UnitOfWorkManager.Generic.cs
+++ FreeSql.DbContext/UnitOfWork/UnitOfWorkManager.Generic.cs
@@ -3,8 +3,12 @@
     /// <summary>
     /// Typed UnitOfWork manager for multi-database scenarios.
     /// </summary>
-    public class UnitOfWorkManager<TMark> : UnitOfWorkManager
+    public class UnitOfWorkManager<TMark> : UnitOfWorkManager, IUnitOfWorkManager<TMark>
     {
         public UnitOfWorkManager(IFreeSql<TMark> fsql) : base(fsql) { }
+    }
+
+    public interface IUnitOfWorkManager<TMark> : IUnitOfWorkManager
+    {
     }
 }
